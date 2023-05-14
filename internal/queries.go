ba5ec977bf7675d82950f438b4247a090f22c196
gus <angusgyoung@gmail.com>
2023-05-14 12:36:16 +0100

    Allow larger keys

--- internal/queries.go
+++ internal/queries.go
@@ -10,10 +10,10 @@ const createTableSql = `
 		id varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid(),
 		created_timestamp timestamp WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
 		updated_timestamp timestamp,
-		status varchar(32) NOT NULL DEFAULT 'PENDING',
+		status varchar(8) NOT NULL DEFAULT 'PENDING',
 		topic varchar(128) NOT NULL,
 		partition smallint NOT NULL,
-		key varchar(36) NOT NULL,
+		key varchar(64) NOT NULL,
 		message bytea NOT NULL,
 		instance_id varchar(36)
 	);
